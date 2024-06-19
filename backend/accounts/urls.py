b3890ad0f4924b56e5036bb287e602b7716634aa
farzan <farzan.70@gmial.com>
2024-06-19 14:03:02 +0330

    add register logic for backend

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,9 @@
-from accounts.views import CustomTokenObtainPairView
+from accounts.views import CustomTokenObtainPairView, RegisterAPIView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 
 urlpatterns = [
     path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/register/", RegisterAPIView.as_view(), name="register"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,53 @@
-from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
 from accounts.serializers import CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class RegisterAPIView(APIView):
+
+    def post(self, request):
+        email_or_mobile = request.data.get("email_or_mobile")
+        password = request.data.get("password")
+
+        user_exists = (
+            User.objects.filter(email=email_or_mobile).exists()
+            or User.objects.filter(mobile=email_or_mobile).exists()
+        )
+
+        if user_exists:
+            return Response(
+                {"error": "User already exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if "@" in email_or_mobile:
+            user = User.objects.create_user(
+                email=email_or_mobile, mobile=None, password=password
+            )
+            user.save()
+        else:
+            user = User.objects.create_user(
+                mobile=email_or_mobile, email=None, password=password
+            )
+            user.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "message": "User created successfully",
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_201_CREATED,
+        )
