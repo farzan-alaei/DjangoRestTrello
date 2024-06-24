c2cbcce4b715d119a934994f339413e4eb1101c5
farzan <farzan.70@gmial.com>
2024-06-24 09:58:30 +0330

    add flowbite to front and edit header and footer

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -1,12 +1,10 @@
 <script>
-    import { onMount } from 'svelte';
-    import { api } from '$lib/api';
-    let profile = {};
-
-    onMount(async () => {
-        profile = await api('profile/');
-    });
+  import { Alert } from 'flowbite-svelte';
 </script>
 
-<h1>Profile Page</h1>
-<p>Email: {profile.email}</p>
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin'
+
 export default {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
+    content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
     darkMode: 'class',
     theme: {
         extend: {
             colors: {
                 primary: {
-                    "50": "#f0fdfa",
-                    "100": "#ccfbf1",
-                    "200": "#99f6e4",
-                    "300": "#5eead4",
-                    "400": "#2dd4bf",
-                    "500": "#14b8a6",
-                    "600": "#0d9488",
-                    "700": "#0f766e",
-                    "800": "#115e59",
-                    "900": "#134e4a",
-                    "950": "#042f2e"
+                    "50": "#f0f9ff",
+                    "100": "#e0f2fe",
+                    "200": "#bae6fd",
+                    "300": "#7dd3fc",
+                    "400": "#38bdf8",
+                    "500": "#0ea5e9",
+                    "600": "#0284c7",
+                    "700": "#0369a1",
+                    "800": "#075985",
+                    "900": "#0c4a6e"
                 }
             },
             fontFamily: {
@@ -57,6 +58,6 @@ export default {
             }
         }
     },
-    plugins: [],
+    plugins: [flowbitePlugin],
 }
 
