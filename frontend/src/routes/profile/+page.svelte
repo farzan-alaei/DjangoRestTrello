d1a84ac546f420dc55571c9d298e236d40dfb85d
farzan <farzan.70@gmial.com>
2024-06-26 09:56:49 +0330

    fix front profile page

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -4,10 +4,9 @@
     import {
         ChartPieSolid,
         GridSolid,
-        MailBoxSolid,
-        UserSolid,
+        ClipboardListSolid,
         ArrowRightToBracketOutline,
-        EditOutline
+        UsersGroupSolid,
     } from 'flowbite-svelte-icons';
 
     $: activeUrl = $page.url.pathname;
@@ -15,34 +14,28 @@
 
 <Sidebar {activeUrl}>
     <SidebarWrapper class="h-96 border-r dark:bg-gray-800 dark:border-gray-700">
-        <SidebarGroup class="space-x-5 mx-8 my-8">
+        <SidebarGroup class="space-x-5 m-8">
             <SidebarItem label="Dashboard" href="/">
                 <svelte:fragment slot="icon">
                     <ChartPieSolid
                             class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
                 </svelte:fragment>
             </SidebarItem>
-            <SidebarItem label="Accordion" href="/docs/components/accordion">
+            <SidebarItem label="Workspaces" href="/docs/components/accordion">
                 <svelte:fragment slot="icon">
                     <GridSolid
                             class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
                 </svelte:fragment>
             </SidebarItem>
-            <SidebarItem label="Alert" href="/docs/components/alert">
+            <SidebarItem label="Boards" href="/docs/components/alert">
                 <svelte:fragment slot="icon">
-                    <MailBoxSolid
+                    <ClipboardListSolid
                             class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
                 </svelte:fragment>
             </SidebarItem>
-            <SidebarItem label="Sidebar" href="/docs/components/sidebar">
+            <SidebarItem label="members" href="/docs/components/sidebar">
                 <svelte:fragment slot="icon">
-                    <UserSolid
-                            class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
-                </svelte:fragment>
-            </SidebarItem>
-            <SidebarItem label="Badge" href="/docs/components/badge">
-                <svelte:fragment slot="icon">
-                    <ArrowRightToBracketOutline
+                    <UsersGroupSolid
                             class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
                 </svelte:fragment>
             </SidebarItem>
